b935909c80cb19fad54817bc4f0ccaddeacc4bae
Andre Fernandez <dev@andrejfernandez.io>
2023-08-30 16:03:39 -0400

    Updated readme

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,7 @@
 	<p class="italic my-5">A minimal, preconfigred setup for creating Websites/Apps using...</p>
 	<ul>
 		<li><a href="https://kit.svelte.dev/" target="_blank">SvelteKit</a></li>
-		<li><a href="https://kit.svelte.dev/" target="_blank">TypeScript</a></li>
+		<li><a href="https://www.typescriptlang.org/" target="_blank">TypeScript</a></li>
 		<li><a href="https://tailwindcss.com/" target="_blank">Tailwind CSS</a></li>
 		<li><a href="https://daisyui.com/" target="_blank">DaisyUI</a></li>
 	</ul>
